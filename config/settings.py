bceaef0194abee8183bae4bf5b1ece28020886c3
rumoza <antani198@gmail.com>
2024-06-30 17:56:34 +0200

    removed keys from settings

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey' # Name for all the SenGrid accounts
-EMAIL_HOST_PASSWORD = "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"
+EMAIL_HOST_PASSWORD = ""
 
 # The email you'll be sending emails from
 DEFAULT_FROM_EMAIL = "jacopo@earthmates.de"
